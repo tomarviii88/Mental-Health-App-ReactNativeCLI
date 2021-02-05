4b79ce91244cd420756ff8722e488e263bfa6870
tomarviii88 <tomarviii88@gmail.com>
2021-02-05 11:38:17 +0530

    project setup

---
+++ App.js
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>App</Text>
+      </View>
+    </NavigationContainer>
+  );
+};
+
+export default App;
